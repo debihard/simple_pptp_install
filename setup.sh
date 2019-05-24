1fd82d2432d5b88a5b4535c41068b26fe0545fca
John <50121345+debihard@users.noreply.github.com>
2019-05-25 00:34:29 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -58,9 +58,9 @@ apt-get -y install pptpd || {
 sed -i 's/#net\.ipv4\.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 
 # IPv6 disabled
-net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.lo.disable_ipv6 = 1
+#net.ipv6.conf.all.disable_ipv6 = 1
+#net.ipv6.conf.default.disable_ipv6 = 1
+#net.ipv6.conf.lo.disable_ipv6 = 1
 sysctl -p
 
 touch /etc/rc.local
